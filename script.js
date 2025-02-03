aacddf7d904deb45eeaa11e32e2ae9f715aa4462
C.Silva <csdesweb@gmail.com>
2025-02-03 17:06:20 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 document.getElementById("modo-toggle").addEventListener("click", function () {
     document.body.classList.toggle("claro");
+    if (document.body.classList.contains("claro")) {
+        localStorage.setItem("modo", "claro");
+    } else {
+        localStorage.removeItem("modo");
+    }
 });
 
 if (localStorage.getItem("modo") === "claro") {
